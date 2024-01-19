06c0e44f90c57418d375188c531e6732907638fc
vinay-1-h <79930534+vinay-1-h@users.noreply.github.com>
2024-01-19 11:11:14 +0530

    Initial Commit

---
+++ CoreDatJson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDatJson
+//
+//  Created by Vinay on 19/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
